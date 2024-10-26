fbfe63b45586697e67e250af6b372df56fe74900
Tejas Devidas Raje <tejas.raje@nagarro.com>
2024-10-26 13:00:10 +0530

    Initial code, create branch from this commit for individual questions

---
+++ src/main/java/com/interview/interview_poc/InterviewPocApplication.java
@@ -0,0 +1,13 @@
+package com.interview.interview_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPocApplication.class, args);
+	}
+
+}
